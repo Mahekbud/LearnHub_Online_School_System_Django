f15c770303d28602a73badda6ee8bf82b5895ea3
Mahek-bud <mahekbud2003@gmail.com>
2024-08-23 14:51:29 +0530

    intial

--- UserAuth/views.py
+++ UserAuth/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
